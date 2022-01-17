63656ccbdae6b9e0761edb295c266526d0575e2d
aleksandr-onopriyenko <new.area.for.study@gmail.com>
2022-01-17 17:18:57 +0200

    finish header block

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,41 @@
-$('.menu__btn').on('click', function () {
-    $(this).toggleClass('active').
+$('.header__burger').on('click', function () {
+    $(this).toggleClass('header__burger--active').
         next().
-        toggleClass('active');
+        toggleClass('header__menu--active');
 });
 
-let $slideItems = $('.carousel-slide');
-let $nextBtn = $('.next.carousel-control');
-let $prevBtn = $('.prev.carousel-control');
-let currentSlide = 0;
+// let $slideItems = $('.carousel-slide');
+// let $nextBtn = $('.next.carousel-control');
+// let $prevBtn = $('.prev.carousel-control');
+// let currentSlide = 0;
 
-let gotoSlide = (n) => {
-    $slideItems[currentSlide].classList.toggle('active');
-    currentSlide = (n + $slideItems.length) % $slideItems.length;
-    $slideItems[currentSlide].classList.toggle('active');
-};
+// let gotoSlide = (n) => {
+//     $slideItems[currentSlide].classList.toggle('active');
+//     currentSlide = (n + $slideItems.length) % $slideItems.length;
+//     $slideItems[currentSlide].classList.toggle('active');
+// };
 
-let nextSlide = () => {
-    gotoSlide(currentSlide + 1);
-};
+// let nextSlide = () => {
+//     gotoSlide(currentSlide + 1);
+// };
 
-let prevSlide = () => {
-    gotoSlide(currentSlide - 1);
-};
+// let prevSlide = () => {
+//     gotoSlide(currentSlide - 1);
+// };
 
-let slideInterval = setInterval(nextSlide, 5000);
+// let slideInterval = setInterval(nextSlide, 5000);
 
-let nextClickHandler = () => {
-    nextSlide();
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, 5000);
-};
+// let nextClickHandler = () => {
+//     nextSlide();
+//     clearInterval(slideInterval);
+//     slideInterval = setInterval(nextSlide, 5000);
+// };
 
-let prevClickHandler = () => {
-    prevSlide();
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, 5000);
-};
+// let prevClickHandler = () => {
+//     prevSlide();
+//     clearInterval(slideInterval);
+//     slideInterval = setInterval(nextSlide, 5000);
+// };
 
-$nextBtn.on('click', nextClickHandler);
-$prevBtn.on('click', prevClickHandler);
+// $nextBtn.on('click', nextClickHandler);
+// $prevBtn.on('click', prevClickHandler);
